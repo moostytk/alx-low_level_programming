480734027ca0fe2c2dd9b8e53e52fdd337d4ad5c
moostytk <almustaphatukur@gmail.com>
2022-12-12 14:03:55 -0800

    completed tasks

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,25 +1,12 @@
 #include <stdio.h>
 
-
-
 /**
+ * main - Entry point
  *
- *  * main - Entry point
- *
- *   * Description: prints "\"Programming is like building a multilingual puzzle"
- *
- *    * Return: 0
- *
- *     */
-
-
-
+ *  Return: Always 0 (Success)
+ */
 int main(void)
-
 {
-
-		puts("\"Programming is like building a multilingual puzzle");
-
-			return (0);
-
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,25 +1,12 @@
 #include <stdio.h>
 
-
-
 /**
+ * main - Entry point
  *
- *  * main - Print a string
- *
- *   * Description: use printf
- *
- *    * Return: 0
- *
- *     */
-
-
-
+ * Return: Always 0 (Success)
+ */
 int main(void)
-
 {
-
-		printf("%s", "with proper grammar, but the outcome is a piece of art,\n");
-
-			return (0);
-
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
