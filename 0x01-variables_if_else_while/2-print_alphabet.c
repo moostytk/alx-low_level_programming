71ce5298a8e703a0dacecbdc4d04b00b666e029b
moostytk <almustaphatukur@gmail.com>
2022-12-17 06:34:15 -0800

    updated

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - main block
+ *  Description: Get a random number and check its last
+ *  Return: 0
+ */
+
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+
+	putchar('\n');
+	return (0);
+}
